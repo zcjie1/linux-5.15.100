98106b6b5dff4838c52cfaf1b0b356da9e87c709
Congjie Zhou <zcjie0802@qq.com>
2024-07-04 22:22:34 +0800

    debug增加devtmpfs挂载

--- debug/prog/busybox_generate.sh
+++ debug/prog/busybox_generate.sh
@@ -1,2 +1,8 @@
-#! /bin/sh
-for i in $(./initramfs/bin/busybox --list); do ln -s ./busybox ./initramfs/bin/$i; done
+#!/bin/bash
+for i in $(./initramfs/bin/busybox --list); do
+    # 检查当前命令是否为 'busybox'
+    if [ "$i" != "busybox" ]; then
+        # 如果不是 'busybox'，则创建软链接
+        ln -s ./busybox ./initramfs/bin/$i
+    fi
+done
